53309e1894160cd2ee2758376fa736e2948d4873
raysayd23 <raysayd23@gmail.com>
2022-02-10 22:57:52 +0100

    challenge 6 finally done !

--- Challenge6.php
+++ Challenge6.php
@@ -1,7 +1,7 @@
 <?php
-    $booksSteven["L'ile au trésor"] = '1883';
-    $booksSteven["Catriona"] = '1893';
-    $booksSteven["Enlevé"] = '1886'; 
+    $booksSteven ["L'ile au trésor"] = '1883';
+    $booksSteven ["Catriona"] = '1893';
+    $booksSteven ["Enlevé"] = '1886'; 
    
     asort($booksSteven);
 
