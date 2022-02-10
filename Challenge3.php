862c0b3df9b498f997e5b27010099522594c8d69
raysayd23 <raysayd23@gmail.com>
2022-02-10 22:50:23 +0100

    challenge 3 done !

---
+++ Challenge3.php
@@ -0,0 +1,12 @@
+<?php
+    $booksSteven["L'ile au trésor"] = '1883';
+    $booksSteven["Catriona"] = '1893';
+    $booksSteven["Enlevé"] = '1886'; 
+   
+    asort($booksSteven);
+
+    foreach ($booksSteven as $key => $value){
+        echo ($key. ' '. $value . '/n');
+    }
+
+    
